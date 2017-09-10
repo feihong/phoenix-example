4ac2306b871b225c9450004f5e3cab54153edf8d
Feihong Hsu <feihong.hsu@gmail.com>
2017-09-09 21:49:51 -0500

    Add login page

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,5 +1,6 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
   def init(opts) do
     # Raise an excpetion if the repo key doesn't exist.
@@ -17,5 +18,22 @@ defmodule RumblWeb.Auth do
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  def login_by_username_and_pass(conn, username, given_pass, opts) do
+    repo = Keyword.fetch!(opts, :repo)
+    user = repo.get_by(Rumbl.User, username: username)
+
+    cond do
+      user && checkpw(given_pass, user.password_hash) ->
+        {:ok, login(conn, user)}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        # Simulate password check with variable timing. This helps prevent
+        # timing attacks.
+        dummy_checkpw()
+        {:error, :not_found, conn}
+    end
   end
 end
